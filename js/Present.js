489df8bca4db76d853f871f42bd9ed7042cff1af
IsseiT <issei.tsuchihashi@gmail.com>
2017-09-21 23:53:59 +0900

    プレゼント画面追加

---
+++ js/Present.js
@@ -0,0 +1,32 @@
+var Present = function() {};
+Present.prototype.show = function() {
+    // お礼メッセージ
+    var message = document.createElement('img');
+    var imgUrl = './images/tyanks_message.png';
+    message.setAttribute('src', imgUrl);
+    message.style.width = '100%';
+    var pass = document.createElement('div');
+    pass.textContent = 'パスワードは４桁の数字'
+    var hint = document.createElement('div');
+    hint.textContent = 'ヒントは『ニュウセキビ』'
+
+    var button = document.createElement('a');
+    var albumUrl = 'http://30d.jp/ayamutoaya/1/photo/1';
+    button.setAttribute('href', albumUrl);
+    button.setAttribute('class', 'square_btn');
+    button.textContent = 'WEDDING PHOTO ALBUM';
+
+
+    // 中央寄せタグ
+    var center = document.createElement('center');
+    center.appendChild(message);
+    center.appendChild(button);
+    center.appendChild(pass);
+    center.appendChild(hint);
+
+    // DOMに追加
+    var content = document.getElementById('content');
+    content.appendChild(center);
+};
+
+window.Present = Present;
